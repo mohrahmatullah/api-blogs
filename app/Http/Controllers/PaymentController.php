62dce00b21493f8e8e510e542b012f2b366b9464
DEVELOP\rahme <mohrahmatullah17051992@gmail.com>
2022-10-17 12:03:36 +0700

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -24,28 +24,41 @@ class PaymentController extends Controller
     public function store(Request $request)
     {
         try {
-            $post = new Payment();
-            $post->user_id          = auth()->user()->id;
-            $post->order_id         = $request->order_id;
-            $post->price            = $request->price;
+            $check_order = Payment::where('order_id',$request->order_id)->first();
+            if($check_order){
+                return response()->json(['status' => 'error', 'message' => 'order id can not be the same']);
+            }
+            else{
+                $post = new Payment();
+                $post->user_id          = auth()->user()->id;
+                $post->order_id         = $request->order_id;
+                $post->price            = $request->price;
 
-            if ($post->save()) {
+                if ($post->save()) {
 
-                $post1 = new Wallet;
-                $post1->user_id = auth()->user()->id;
-                $post1->currency_id = $request->currency_id;
-                $post1->amount = $request->price;
-                $post1->save();
+                    $post1_update = Wallet::where('user_id',auth()->user()->id)->first();
+                    if($post1_update){
+                        $post1_update->amount = $request->price + $post1_update->amount;
+                        $post1_update->save(); 
+                    }
+                    else{                    
+                        $post1 = new Wallet;
+                        $post1->user_id = auth()->user()->id;
+                        $post1->currency_id = $request->currency_id;
+                        $post1->amount = $request->price;
+                        $post1->save();
+                    }
 
-                $post3= new Transaction;
-                $post3->payment_id = $post->id;
-                $post3->wallet_id = $post1->id;
-                $post3->merchant_id = $request->merchant_id;
-                $post3->description = $request->description;
-                $post3->status = 'pending';
-                $post3->save();
+                    $post3= new Transaction;
+                    $post3->payment_id = $post->id;
+                    $post3->wallet_id = $post1->id ?? $post1_update->id;
+                    $post3->merchant_id = $request->merchant_id;
+                    $post3->description = $request->description;
+                    $post3->status = 'pending';
+                    $post3->save();
 
-                return response()->json(['status' => 'success', 'message' => 'Payment successfully']);
+                    return response()->json(['status' => 'success', 'message' => 'Payment successfully']);
+                }                
             }
         } catch (\Exception $e) {
             return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
